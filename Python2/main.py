8084ed4ef78d64060eb948972802e373166ea2b9
JimPaan <jimpaan9070@gmail.com>
2023-01-19 21:27:03 +0800

    Update main.py

--- Python2/main.py
+++ Python2/main.py
@@ -1,39 +1,38 @@
-import sys
-
 from PyQt5.QtWidgets import *
 
-from MainPage import Ui_MainPage
 from Display import Ui_Display
+from MainPage import Ui_MainPage
 
 
 class first_window(QWidget, Ui_MainPage):
     
-    def __int__(self):
-        super(first_window, self).__int__()
+    def __init__(self):
+        super(first_window, self).__init__()
         self.setupUi(self)
+        self.this = second_window()
         self.pushButton.clicked.connect(self.gotosecond)
 
     def gotosecond(self):
-        this = second_window()
-        this.show()
+        self.this.show()
         self.hide()
 
 
 class second_window(QWidget, Ui_Display):
 
-    def __int__(self):
-        super(second_window, self).__int__()
+    def __init__(self):
+        super(second_window, self).__init__()
         self.setupUi(self)
-        self.BackButton.clicked.connect(self.gotofirst)
+        #self.that = first_window()
+        self.BackButton.clicked.connect(self.hide)
 
-    def gotofirst(self):
-        that = first_window()
-        that.show()
+
+"""    def gotofirst(self):
+        self.that.show()
         self.hide()
-
+"""
 
 if __name__ == '__main__':
     app = QApplication([])
     window = first_window()
     window.show()
-    sys.exit(app.exec_())
+    app.exec_()
