cc39a03ad759435e7e32e8b93a011b480d88dd34
AdeebZ-tech <adeebzikri0@gmail.com>
2023-01-22 10:30:55 +0800

    database

--- Python2/warehouseTracker/dashboard/models.py
+++ Python2/warehouseTracker/dashboard/models.py
@@ -4,11 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    idnum = models.TextField(max_length=100, null=True)
+    idnum = models.TextField(max_length=15, null=True)
     dateIn = models.DateField(max_length=100, null=True)
     dateOut = models.DateField(max_length=100, null=True)
     Stats = models.CharField(max_length=100, null=True)
 
+    def __str__(self):
+        return f'{self.idnum}{self.dateIn}{self.dateOut}{self.Stats}'
 
 
 
